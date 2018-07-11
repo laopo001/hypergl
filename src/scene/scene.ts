47f7df1abf607ca35ad44a4b98e16dbbe18eca8b
liaodh <liaodh@jpush.com>
2018-07-11 21:20:37 +0800

    add(math): 添加math文件

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -5,13 +5,16 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Wednesday, July 11th 2018, 8:55:28 pm
+ * Last Modified: Wednesday, July 11th 2018, 9:11:08 pm
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
 
+import { Component } from '../core/component';
+export class Scene extends Component {
+    fog;
+    add() {
 
-export class Scene {
-
+    }
 }
