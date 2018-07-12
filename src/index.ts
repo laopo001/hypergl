9eb0991584e11b8d92e5d4dee7a4098bbc95ee69
laopo001 <353272497@qq.com>
2018-07-13 01:22:18 +0800

    feat(vertexFormat): 添加顶点格式化

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Thursday, July 12th 2018, 8:24:06 pm
+ * Last Modified: Friday, July 13th 2018, 12:59:31 am
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
@@ -20,5 +20,32 @@ namespace HGL {
     export const BUFFER_STREAM = 3;
     export const BUFFER_GPUDYNAMIC = 4;
 
+
+    export enum DataType {
+        INT8 = 0,
+        UINT8 = 1,
+        INT16 = 2,
+        UINT16 = 3,
+        INT32 = 4,
+        UINT32 = 5,
+        FLOAT32 = 6
+    }
+
+    export enum SEMANTIC {
+        POSITION = "POSITION",
+        NORMAL = "NORMAL",
+        TANGENT = "TANGENT",
+        BLENDWEIGHT = "BLENDWEIGHT",
+        BLENDINDICES = "BLENDINDICES",
+        COLOR = "COLOR",
+        TEXCOORD0 = "TEXCOORD0",
+        TEXCOORD1 = "TEXCOORD1",
+        TEXCOORD2 = "TEXCOORD2",
+        TEXCOORD3 = "TEXCOORD3",
+        TEXCOORD4 = "TEXCOORD4",
+        TEXCOORD5 = "TEXCOORD5",
+        TEXCOORD6 = "TEXCOORD6",
+        TEXCOORD7 = "TEXCOORD7"
+    }
 
 }
