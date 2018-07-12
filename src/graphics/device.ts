10f010ac0aa8ce9ca47b0da523e57a176cd5ddc5
laopo001 <353272497@qq.com>
2018-07-12 19:53:23 +0800

    add(devicde,application): add files

---
+++ src/graphics/device.ts
@@ -0,0 +1,14 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\src\graphics\device.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Thursday, July 12th 2018, 7:49:57 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Thursday, July 12th 2018, 7:50:00 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
