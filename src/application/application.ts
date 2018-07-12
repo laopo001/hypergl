c8ebb562011a206b3dca76a5f8026c4430cab36f
liaodh <liaodh@jpush.com>
2018-07-12 19:53:23 +0800

    add(devicde,application): add files

---
+++ src/application/application.ts
@@ -0,0 +1,19 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\src\application\application.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Thursday, July 12th 2018, 7:51:04 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Thursday, July 12th 2018, 7:52:28 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+export class Application {
+    constructor(canvas: HTMLCanvasElement, option: any) {
+
+    }
+}
