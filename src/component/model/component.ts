0d5b4ec18250cd4ab75cec31bc309e3de73cbb0e
liaodh <liaodh@jpush.com>
2018-07-12 19:32:47 +0800

    add(graphics): 添加文件夹

---
+++ src/component/model/component.ts
@@ -0,0 +1,21 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\src\component\model\model.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Thursday, July 12th 2018, 6:40:29 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Thursday, July 12th 2018, 7:05:05 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+export class Model {
+    defaultMaterial;
+    meshInstances;
+    castShadows = true;
+    receiveShadows = true;
+    isStatic = false;
+}
