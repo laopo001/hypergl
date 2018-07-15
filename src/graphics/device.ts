6b51cd2c974510912bc8d8c786a3bf4575c61e66
laopo001 <353272497@qq.com>
2018-07-15 19:07:43 +0800

    fix

--- src/graphics/device.ts
+++ src/graphics/device.ts
@@ -5,7 +5,7 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Sunday, July 15th 2018, 11:59:00 am
+ * Last Modified: Sunday, July 15th 2018, 6:13:43 pm
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
@@ -16,6 +16,7 @@ export class GraphicsDevice {
     gl: WebGLRenderingContext;
     webgl2: boolean = false;
     buffers = [];
+    precision: 'mediump' = 'mediump';
     constructor(private canvas: HTMLCanvasElement) {
         this.gl = canvas.getContext('webgl');
     }
