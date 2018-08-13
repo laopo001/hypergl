6b125d37df7ef4f6c010a2ff0a6658429fa09049
laopo001 <353272497@qq.com>
2018-08-13 20:55:23 +0800

    add mesh_new.ts

---
+++ src/mesh/mesh_new.ts
@@ -0,0 +1,17 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\src\mesh\mesh_new.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Monday, August 13th 2018, 8:54:48 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Monday, August 13th 2018, 8:55:06 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+export class Mesh {
+
+}
