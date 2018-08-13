b17479ed294b07f0fc790fb7d4a9c44952218b40
laopo001 <353272497@qq.com>
2018-08-13 20:58:50 +0800

    add unit test

---
+++ tests/mesh/mesh.test.ts
@@ -0,0 +1,14 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\tests\mesh\mesh.test.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Monday, August 13th 2018, 8:57:29 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Monday, August 13th 2018, 8:57:34 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
