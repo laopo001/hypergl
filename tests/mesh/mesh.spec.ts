4c745476d3fcf55c7169678efa95aa5dc2c30720
laopo001 <353272497@qq.com>
2018-08-14 12:17:49 +0800

    add ts-jest and pre-commit run test

--- tests/mesh/mesh.spec.ts
+++ tests/mesh/mesh.spec.ts
@@ -5,10 +5,14 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Monday, August 13th 2018, 8:57:34 pm
- * Modified By: liaodh
+ * Last Modified: Tuesday, August 14th 2018, 12:13:02 pm
+ * Modified By: dadigua
  * -----
  * Copyright (c) 2018 jiguang
  */
 
 
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
