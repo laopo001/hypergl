1ad0c6120115ca97cf921cd5ce0b1cd97be61ae7
laopo001 <353272497@qq.com>
2018-08-14 17:57:20 +0800

    add(conf,vertexFormat...)

---
+++ src/conf.ts
@@ -0,0 +1,54 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \src\conf.ts
+ * Created Date: Tuesday, August 14th 2018, 5:02:06 pm
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, August 14th 2018, 5:02:19 pm
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+
+/**
+ * 数据类型
+ *
+ * @export
+ * @enum {number}
+ */
+export enum DataType {
+    INT8,
+    UINT8,
+    INT16,
+    UINT16,
+    INT32,
+    UINT32,
+    FLOAT32
+}
+
+
+/**
+ * 顶点数据输入定义
+ *
+ * @export
+ * @enum {number}
+ */
+export enum SEMANTIC {
+    POSITION = 'POSITION',
+    NORMAL = 'NORMAL',
+    TANGENT = 'TANGENT',
+    BLENDWEIGHT = 'BLENDWEIGHT',
+    BLENDINDICES = 'BLENDINDICES',
+    COLOR = 'COLOR',
+    TEXCOORD0 = 'TEXCOORD0',
+    TEXCOORD1 = 'TEXCOORD1',
+    TEXCOORD2 = 'TEXCOORD2',
+    TEXCOORD3 = 'TEXCOORD3',
+    TEXCOORD4 = 'TEXCOORD4',
+    TEXCOORD5 = 'TEXCOORD5',
+    TEXCOORD6 = 'TEXCOORD6',
+    TEXCOORD7 = 'TEXCOORD7'
+}
---
+++ src/graphics/vertexFormat.ts
@@ -0,0 +1,14 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \src\graphics\vertexFormat.ts
+ * Created Date: Tuesday, August 14th 2018, 5:02:37 pm
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, August 14th 2018, 5:02:40 pm
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
