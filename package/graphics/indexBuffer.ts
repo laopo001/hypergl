7d24a39d741ef253ebbb3d24d6c72eb0a28ad06f
laopo001 <353272497@qq.com>
2018-08-14 10:34:02 +0800

    fix

---
+++ package/graphics/indexBuffer.ts
@@ -0,0 +1,14 @@
+/**
+ * FileName: indexBuffer.ts
+ * Path: \package\graphics\indexBuffer.ts
+ * Created Date: Tuesday, August 14th 2018, 10:25:14 am
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, August 14th 2018, 10:25:19 am
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
---
+++ package/graphics/vertexBuffer.ts
@@ -0,0 +1,14 @@
+/**
+ * File: c:\Users\35327\Documents\Githubs\hypergl\package\graphics\vertexBuffer.ts
+ * Project: c:\Users\35327\Documents\Githubs\hypergl
+ * Created Date: Tuesday, August 14th 2018, 10:14:54 am
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, August 14th 2018, 10:15:05 am
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
