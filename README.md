830eb3d33623020fb23f82a4982578ff2947b847
laopo001 <353272497@qq.com>
2018-08-22 01:51:12 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # working[![Build Status](https://travis-ci.org/laopo001/hypergl.svg?branch=master)](https://travis-ci.org/laopo001/hypergl)
 
+模块 | 功能
+---|---
+应用程式模块| 场景管理场景切换
+场景模块 | 管理光源、摄像机和模型等
+资源加载工具模块| 请求资源和管理资源I/O功能
+图形渲染模块|用来执行渲染和画面输出
+输入管理模块|用来获取用户输入
+
+
--- src/application.ts
+++ src/application.ts
@@ -5,7 +5,7 @@
  * @author: dadigua
  * @summary: short description for the file
  * -----
- * Last Modified: Tuesday, August 21st 2018, 6:50:35 pm
+ * Last Modified: Wednesday, August 22nd 2018, 1:50:27 am
  * Modified By: dadigua
  * -----
  * Copyright (c) 2018 jiguang
@@ -15,7 +15,7 @@
 import { Scene } from './scene/scene';
 import { RendererPlatform } from './graphics/renderer';
 import { AppOption } from './types';
-
+import { event } from './core/event';
 export class Application {
     sceneInstances: Scene[] = [];
     activeIndex = 0;
@@ -28,15 +28,21 @@ export class Application {
         this.canvas = canvas;
         this.rendererPlatform = new RendererPlatform(this.canvas);
         this.sceneInstances.push(new Scene(this));
+        this.start();
     }
     start() {
-        window.requestAnimationFrame(this.tick);
+        this.tick();
     }
     add(scene: Scene) {
         this.sceneInstances.push(scene);
     }
+    addEventListen(cb) {
+        event.on('update', cb);
+    }
     private tick() {
-        this.scene.renderer();
+        // this.scene.renderer();
+        event.fire('update');
+        window.requestAnimationFrame(this.tick.bind(this));
     }
 
     private complete() {
