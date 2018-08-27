236c00bbf3a3c2a75e9c652ba8e318f3ecfc75fd
laopo001 <353272497@qq.com>
2018-08-28 01:22:11 +0800

    start entity

---
+++ src/component/entity.ts
@@ -0,0 +1,18 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \src\component\entity.ts
+ * Created Date: Tuesday, August 28th 2018, 1:21:15 am
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, August 28th 2018, 1:21:50 am
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+import { INode } from '../scene/node';
+export class Entity extends INode {
+
+}
