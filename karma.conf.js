ae7642e556a48d01c963abaef9059d5ced5e094f
laopo001 <353272497@qq.com>
2018-09-05 01:53:45 +0800

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@
  * @author: dadigua
  * @summary: short description for the file
  * -----
- * Last Modified: Wednesday, September 5th 2018, 1:40:14 am
+ * Last Modified: Wednesday, September 5th 2018, 1:53:34 am
  * Modified By: dadigua
  * -----
  * Copyright (c) 2018 jiguang
@@ -50,13 +50,53 @@ module.exports = function (config) {
             enabled: true,
             usePhantomJS: false
         },
+        detectBrowsers: {
+            // enable/disable, default is true
+            enabled: true,
 
+            // enable/disable phantomjs support, default is true
+            // usePhantomJS: true,
+
+            // use headless mode, for browsers that support it, default is false
+            // preferHeadless: true,
+
+            // post processing of browsers list
+            // here you can edit the list of browsers used by karma
+            postDetection: function (availableBrowsers) {
+                /* Karma configuration with custom launchers
+                  customLaunchers: {
+                    IE9: {  https://github.com/litixsoft/karma-detect-browsers
+                      base: 'IE',
+                      'x-ua-compatible': 'IE=EmulateIE9'
+                    }
+                  }
+                */
+
+                //Add IE Emulation
+                var result = availableBrowsers;
+
+                // if (availableBrowsers.indexOf('IE') > -1) {
+                //     result.push('IE9');
+                // }
+
+                //Remove PhantomJS if another browser has been detected
+                if (availableBrowsers.length > 1 && availableBrowsers.indexOf('IE') > -1) {
+                    var i = result.indexOf('IE');
+
+                    if (i !== -1) {
+                        result.splice(i, 1);
+                    }
+                }
+
+                return result;
+            }
+        },
         plugins: [
             'karma-mocha',
             'karma-typescript',
             'karma-chrome-launcher',
             'karma-firefox-launcher',
-            // 'karma-ie-launcher',
+            'karma-ie-launcher',
             'karma-safari-launcher',
             'karma-detect-browsers'
         ]
