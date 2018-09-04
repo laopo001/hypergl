9551d4279e0f3417a424f9fc802946adba05f8ac
laopo001 <353272497@qq.com>
2018-09-05 01:09:59 +0800

    add light.ts

---
+++ src/lights/light.ts
@@ -0,0 +1,14 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \src\lights\light.ts
+ * Created Date: Tuesday, September 4th 2018, 11:04:18 pm
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Tuesday, September 4th 2018, 11:04:19 pm
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
