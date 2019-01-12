3af91497764f69f39e1d6e672634ab1ff2e07370
laopo001 <353272497@qq.com>
2019-01-12 15:22:23 +0800

    add ammo.js

---
+++ plugins/physics/ammo.ts
@@ -0,0 +1,21 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \plugins\physics\ammo.ts
+ * Created Date: Saturday, January 12th 2019, 2:08:21 pm
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Saturday, January 12th 2019, 3:10:25 pm
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2019 dadigua
+ */
+
+
+import * as Ammo from 'laopo001-ammo';
+// import * as Ammo from './ammo';
+import { Application, Plugin, Vec3, Quat, Entity } from 'hypergl';
+
+export class AmmoPlugin implements Plugin {
+    static pname = 'ammo';
+}
---
+++ plugins/physics/types.ts
@@ -0,0 +1,17 @@
+/*
+ * ProjectName: hypergl
+ * FilePath: \plugins\physics\types.ts
+ * Created Date: Saturday, January 12th 2019, 2:19:45 pm
+ * @author: dadigua
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Saturday, January 12th 2019, 2:20:04 pm
+ * Modified By: dadigua
+ * -----
+ * Copyright (c) 2019 dadigua
+ */
+
+
+interface IPhysics {
+
+}
