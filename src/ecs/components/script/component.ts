3f782d0eee07f814e92e4804efa577fa6d76a744
laopo001 <353272497@qq.com>
2019-02-26 16:59:32 +0800

    fix script.ts

--- src/ecs/components/script/component.ts
+++ src/ecs/components/script/component.ts
@@ -5,7 +5,7 @@
  * @author: dadigua
  * @summary: short description for the file
  * -----
- * Last Modified: Monday, February 25th 2019, 11:55:22 am
+ * Last Modified: Tuesday, February 26th 2019, 1:47:49 pm
  * Modified By: dadigua
  * -----
  * Copyright (c) 2018 dadigua
@@ -41,12 +41,12 @@ export class ScriptComponent extends Component<ScriptInputs> {
             script.initialize();
         });
         this.entity.scene.event.on('update', this.update);
-        // event.on('update', this.update);
+        event.on('update', this.app_update);
     }
     destroy() {
         super.destroy();
         this.entity.scene.event.off('update', this.update);
-        // event.off('update', this.update);
+        event.off('update', this.app_update);
         this.instance.forEach(script => {
             script.entity = this.entity;
             // tslint:disable-next-line:no-unused-expression
@@ -60,7 +60,8 @@ export class ScriptComponent extends Component<ScriptInputs> {
     }
     private app_update = (e: number) => {
         this.instance.forEach(script => {
-            script.update(e);
+            // tslint:disable-next-line:no-unused-expression
+            script.appUpdate && script.appUpdate(e);
         });
     }
 }
